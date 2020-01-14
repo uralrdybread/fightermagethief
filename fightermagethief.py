78956a9847473d26b6558160060dce3545342b36
uralrdybread <48900828+uralrdybread@users.noreply.github.com>
2020-01-14 13:43:06 -0800

    Update fightermagethief.py

--- fightermagethief.py
+++ fightermagethief.py
@@ -1,40 +1,52 @@
-
-# coding: utf-8
-
-# In[6]:
-
-
 from random import randint
 
-player = input("Player, please choose the Fighter, Mage or Thief: ").lower()
-rand_num = randint(0,2)
-if rand_num == 0:
-    opponent = "fighter"
-elif rand_num == 1:
-    opponent = "mage"
-else:
-    opponent = "thief"
-    
-print(f"Opponent plays {opponent}" )
+player_wins = 0
+opponent_wins = 0
+# winning_score = 
+# Added option for win condition other than best out of 3
 
-if player == opponent:
-    print("The fight is a draw!")
-elif player == "fighter":
-    if opponent == "thief":
-        print("The Fighter stands triumphant over the Thief")
+while player_wins < 2 and opponent_wins < 2:
+    print(f"Player Score: {player_wins} Opponent Score: {opponent_wins}")
+    player = input("Player, please choose the Fighter, Mage or Thief: ").lower()
+    if player == "quit" or player == "q":
+        break
+    rand_num = randint(0,2)
+    if rand_num == 0:
+        opponent = "fighter"
+    elif rand_num == 1:
+        opponent = "mage"
     else:
-        print("The Fighter is no match for the Mage's arcane prowess")
-elif player == "mage":
-    if opponent == "fighter":
-        print("The Fighter is no match for the Mage's arcane prowess")
-    else:
-        print("The Mage is overwhelmed by the Thief's skill and cunning")
-elif player == "thief":
-    if opponent == "fighter":
-        print("The fighter stands triumphant over the thief")
-    else:
-        print("The Mage is overwhelmed by the thief's skill and cunning")
-else:
-    print("Please choose either the Fighter, Mage or Thief!")
-    
+        opponent = "thief"
 
+    print(f"Opponent plays {opponent}" )
+
+    if player == opponent:
+        print("The fight is a draw!")
+    elif player == "fighter":
+        if opponent == "thief":
+            print("The Fighter stands triumphant over the Thief")
+            player_wins += 1
+        else:
+            print("The Fighter is no match for the Mage's arcane prowess")
+            opponent_wins += 1
+    elif player == "mage":
+        if opponent == "fighter":
+            print("The Fighter is no match for the Mage's arcane prowess")
+            player_wins += 1
+        else:
+            print("The Mage is overwhelmed by the Thief's skill and cunning")
+            opponent_wins += 1
+    elif player == "thief":
+        if opponent == "fighter":
+            print("The fighter stands triumphant over the thief")
+            opponent_wins += 1
+        else:
+            print("The Mage is overwhelmed by the thief's skill and cunning")
+            player_wins += 1
+    else:
+        print("Please choose either the Fighter, Mage or Thief!")
+if player_wins > opponent_wins:
+    print("You are victorious!")
+else:
+    print("You have been defeated...")
+print(f"BATTLE RESULTS... Player Score: {player_wins} Opponent Score: {opponent_wins}")
